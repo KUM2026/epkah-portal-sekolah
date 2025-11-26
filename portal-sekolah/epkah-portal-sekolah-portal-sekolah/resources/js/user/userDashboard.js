53124a529b52417e96f74fff2537d790d46c8661
iffahmustari <iffahmustari050@gmail.com>
2025-11-26 11:35:28 +0800

    Remove old portal-sekolah folder

--- portal-sekolah/epkah-portal-sekolah-portal-sekolah/resources/js/user/userDashboard.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    // Contoh: alert bila klik "Hantar Kitar Semula"
-    const btn = document.querySelector(".btn-yellow");
-    if (btn) {
-        btn.addEventListener("click", () => {
-            alert("Fungsi Hantar Kitar Semula akan datang!");
-        });
-    }
-});
--- portal-sekolah/epkah-portal-sekolah-portal-sekolah/resources/views/partials/_header.blade.php
+++
@@ -1,4 +0,0 @@
-<header class="bg-gradient-to-r from-green-700 to-green-500 text-white text-center py-12 rounded-xl mx-8 mt-8 shadow-lg">
-        <h1 class="text-4xl font-extrabold mb-2 drop-shadow">Program Komuniti Amalan Hijau Kelantan (PKAH)</h1>
-        <p class="mt-2 text-lg font-medium">Inisiatif Memperkasakan Pendidikan Kelestarian Kepada Ekonomi Kitaran Di Negeri Kelantan</p>
-    </header>
